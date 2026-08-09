d407f3c8369ca1f7166ab6529cdd3fd05692da3a
Aditya <adityac0104@gmail.com>
2026-08-09 14:22:48 +0530

    Restructuring the folder

--- Backend/src/Middlewares/auth.middleware.js
+++ Backend/src/Middlewares/auth.middleware.js
@@ -1,11 +1,17 @@
 const jwt = require('jsonwebtoken');
+const tokenBlacklistModel = require('../models/blacklist.model');
 
 
-function authenticateToken(req, res, next) {
+async function authenticateToken(req, res, next) {
   const token = req.cookies.token;
 
   if (!token) {
     return res.status(401).json({ message: 'Access Denied. No token provided.' });
+  }
+
+  const isBlacklisted = await tokenBlacklistModel.findOne({ token });
+  if (isBlacklisted) {
+    return res.status(401).json({ message: 'Access Denied. Token is invalid.' });
   }
 
 try {
