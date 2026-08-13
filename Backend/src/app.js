15081ea122f12e86cbce1bb69be0acd1b0bd2812
Aditya <adityac0104@gmail.com>
2026-08-13 23:46:21 +0530

    Changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-
-
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
-
-
-const authRoutes = require('./routes/auth.routes');//require all the auth routes here
-
-app.use('/api/auth', authRoutes);//use all the auth routes here
+const authRoutes = require('./routes/auth.routes');
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
