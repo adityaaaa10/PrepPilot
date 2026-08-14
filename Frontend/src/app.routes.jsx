eddcb060241beba33ee602374232528058a5a3fa
Aditya <adityac0104@gmail.com>
2026-08-14 18:54:31 +0530

    protrcted home page

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,18 +1,17 @@
-import {createBrowserRouter} from "react-router";
+import { createBrowserRouter } from "react-router";
 import Register from "./features/auth/pages/register";
 import Login from "./features/auth/pages/login";
+import ProtectedRoute from "./features/auth/components/protected";
 
 export const router = createBrowserRouter([
-    {
-        path: "/register",
-        element: <Register />
-    },
-    {
-        path: "/login",
-        element: <Login />
-    },
-    {
-        path: "/",
-        element: <h1>Home page</h1>
-    }
-])
+  { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login /> },
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <h1>Home Page</h1>
+      </ProtectedRoute>
+    ),
+  },
+]);
